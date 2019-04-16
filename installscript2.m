ef9ce6b8238b6dab6fca4cef59ebd6fa6828dd7b
elecu <ed_in@outlook.com>
2019-04-16 13:58:53 -0500

    Update installscript2.m

--- installscript2.m
+++ installscript2.m
@@ -1,4 +1,4 @@
 (* ::Package:: *)
 
 (* ::Input:: *)
-(*SpaceMath:= SetDirectory@FileNameJoin[{$UserBaseDirectory,"Applications"}]  URLSave["https://github.com/elecu/install_test/raw/master/APPStestI.zip","APPStestI.zip"]   ExtractArchive["APPStestI.zip"]  DeleteFile["APPStestI.zip"]*)
+(*InstallSpaceMath:= SetDirectory@FileNameJoin[{$UserBaseDirectory,"Applications"}]  URLSave["https://github.com/elecu/install_test/raw/master/APPStestI.zip","APPStestI.zip"]   ExtractArchive["APPStestI.zip"]  DeleteFile["APPStestI.zip"]*)
