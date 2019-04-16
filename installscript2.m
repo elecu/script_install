cca06c86f0754e4b2a5a9b57fb89a5aea998be5a
elecu <ed_in@outlook.com>
2019-04-16 13:21:51 -0500

    Add files via upload

--- installscript2.m
+++ installscript2.m
@@ -1,24 +1,4 @@
 (* ::Package:: *)
 
 (* ::Input:: *)
-(*InstallSpaceMath[]:={*)
-(**)
-(*SetDirectory@FileNameJoin[{$UserBaseDirectory,"Applications"}]*)
-(**)
-
-
-(* ::Input:: *)
-(*URLSave["https://github.com/elecu/install_test/raw/master/APPStestI.zip","APPStestI.zip"];*)
-
-
-(* ::Input:: *)
-(**)
-(*ExtractArchive["APPStestI.zip"];*)
-
-
-(* ::Input:: *)
-(*DeleteFile["APPStestI.zip"];}*)
-
-
-(* ::Input:: *)
-(**)
+(*SpaceMath:={SetDirectory@FileNameJoin[{$UserBaseDirectory,"Applications"}]  URLSave["https://github.com/elecu/install_test/raw/master/APPStestI.zip","APPStestI.zip"]   ExtractArchive["APPStestI.zip"]  DeleteFile["APPStestI.zip"];}*)
